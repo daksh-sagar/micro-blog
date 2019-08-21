b7a3c96f194b0137530ce121cf3069ec9ed12c79
Daksh <daksh.sagar@yahoo.com>
2019-08-21 23:58:23 +0530

    create post model

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,26 @@
 const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Post must have a Title'],
+    trim: true
+  },
+  body: {
+    type: String,
+    required: [true, 'Post must have some text'],
+    trim: true
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
